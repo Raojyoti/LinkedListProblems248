bbdf876850913997aabe23cce3b9b16aa910a83e
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-02-08 14:41:51 +0530

    [Jyoti Rao] Create a Linked list

---
+++ LinkedListProblems/LinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblems
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void AddLast(int data)
+        {
+            Node newnode = new Node(data);
+            if(head == null)
+            {
+                head = newnode;
+                Console.WriteLine("{0} node inserted into LinkedList", newnode.data);
+            }
+            else
+            {
+                Node temp=head;
+                while(temp.next != null)
+                {
+                    temp= temp.next;
+                }
+                temp.next = newnode;
+                Console.WriteLine("{0} node inserted into LinkedList", newnode.data);
+            }
+        }
+        public void Display()
+        {
+            if(head== null)
+            {
+                Console.WriteLine("LinkedList is empty");
+            }
+            else
+            {
+                Node temp =head;
+                Console.WriteLine("\nNodes present in LinkedList:\n----------------------------");
+                while (temp != null)
+                {
+                    Console.Write(temp.data+" ");
+                    temp =temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ LinkedListProblems/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LinkedListProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Console.WriteLine("Welcome to the LinkedList Problems\n-----------------------------");
+            Console.WriteLine("First please select options");
+            Console.WriteLine("1.CreateLinkedList\n2.Exit\n");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            { 
+                case 1:
+                    Console.Clear();
+                    linkedList.AddLast(56);
+                    linkedList.AddLast(30);
+                    linkedList.AddLast(70);
+                    linkedList.Display();
+                    break; 
+                case 2:
+                    Console.Clear();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Please select only given options");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
